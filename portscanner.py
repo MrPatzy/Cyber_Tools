aa50b396b0ea359ce44ac4c188ddca8486aa06e8
Zachary "Patzy" Patz <93362679+MrPatzy@users.noreply.github.com>
2022-11-14 22:30:14 -0700

    Updated PortScanner Code

--- portscanner.py
+++ portscanner.py
@@ -1,37 +1,27 @@
+import argparse
 import socket
 import subprocess
 import sys
-import getopt
-import argparse
 from datetime import datetime
 
 #Blank your screen
 subprocess.call('clear', shell=True)
 
-#argumentList = sys.argv[1:]
+top20 = [21,22,23,25,53,80,110,111,135,139,143,443,445,993,995,1723,3306,3389,5900,8080]
+top200 = [1,3,7,9,13,17,19,21,22,23,25,26,37,53,79,80,81,82,88,100,106,110,111,113,119,135,139,143,144,179,199,254,255,280,311,389,427,443,444,445,464,465,497,513,514,515,543,544,548,554,587,593,625,631,636,646,787,808,873,902,990,993,995,1000,1022,1024,1025,1026,1027,1028,1029,1030,1031,1032,1033,1035,1036,1037,1038,1039,1040,1041,1044,1048,1049,1050,1053,1054,1056,1058,1059,1064,1065,1066,1069,1071,1074,1080,1110,1234,1433,1494,1521,1720,1723,1755,1761,1801,1900,1935,1998,2000,2001,2002,2003,2005,2049,2103,2105,2107,2121,2161,2301,2383,2401,2601,2717,2869,2967,3000,3001,3128,3268,3306,3389,3689,3690,3703,3986,4000,4001,4045,4899,5000,5001,5003,5009,5050,5051,5060,5101,5120,5190,5357,5432,5555,5631,5666,5800,5900,5901,6000,6001,6002,6004,6112,6646,6666,7000,7070,7937,7938,8000,8002,8008,8009,8010,8031,8080-8081,8443,8888,9000-9001,9090,9100,9102,9999-10001,10010,32768,32771,49152-49157,50000]
 
-#Options
-#options = "hr:o:"
-
-#long Options
-#longOptions = ["help", "range=", "output="]
-
-#outputFile = None
-
-parser = argparse.ArgumentParser(prog='portscanner', description="Home grown port scanner.")
+parser = argparse.ArgumentParser(prog='portscanner', 
+                                description="Home grown port scanner.",
+                                epilog='PortScanner v 1.0 Written by Patzy')
 
 #IP Address
-parser.add_argument('IP Address', 
-                    metavar='IP',
-                    help='IP address, IP range, or CIDR range to be scanned.',
-                    nargs='+')
+parser.add_argument("address", help='IP address, IP range, or CIDR range to be scanned.')
 
 #Output Flag
 parser.add_argument('-o', '--output',
                     help='File name to write output to',
                     nargs=1,
-                    type=str,
-                    dest='ofile')
+                    type=str)
 
 #Verbosity Flag
 parser.add_argument('-v', '--verbose',
@@ -43,45 +33,22 @@ parser.add_argument('-v', '--verbose',
 #Selecting Port Range
 parser.add_argument('-r', '--range',
                     nargs=2, 
-                    default=[1-5000],
+                    default= list(range(1,5000)),
                     type=int,
-                    help='Define the range of ports to scan. Default is 1-5000 \n If you need to scan specific ports, see the -sp option',
+                    help='Define the range of ports to scan. Default is 1-5000 /n If you need to scan specific ports, see the -sp option',
                     dest='range')
 
 #Scan Specific Ports
-parser.add_argument('-sp', '--s.ports'
-                    nargs='+'
+parser.add_argument('-sp', '--s.ports',
+                    nargs='+',
                     type=int,
-                    help='Choose specific ports to scan. ',
-                    dest='s.port')                    
+                    help='Choose specific ports to scan. ')                    
 
-
-
-
-#try:
-    #Parsing Arguments
- #   arguments, values = getopt.getopt(argumentList, options, longOptions)
-
-    #checking each argument
-  #  for currentArgument, currentValue in arguments:
-   #     if currentArgument in ("-h", "--help"):
-    #        print ("Displaying Help")
-     #       sys.exit()
-      #  elif currentArgument in ("-r", "--range"):
-       #     print (("Port Range Selected (% s)") % currentValue)
-        #elif currentArgument in ("-o", "--output"):
-         #   print (("Output file set as %s") % currentValue)
-          #  outputFile = currentValue
-
-#except getopt.error as err:
- #   #output error, and return with an error code
-  #  print (str(err))
-
-#Ask for input
+args = parser.parse_args()
 
 print ("Welcome to Patzy's Port Scanner")
-scanServer = input("Enter a remote host to scan: ")
-
+#scanServer = input("Enter a remote host to scan: ")
+scanServer = args.address
 scanServerIP = socket.gethostbyname(scanServer)
 
 
@@ -97,7 +64,7 @@ t1 = datetime.now()
 #Also we will do error handling
 
 try:
-        for port in range (1,5000):
+        for port in args.range:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             result = sock.connect_ex((scanServerIP, port))
             if result == 0:
